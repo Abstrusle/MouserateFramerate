469d99ce3d158e196df290fa17fc308d6de3b803
Abstrusle <40145743+Abstrusle@users.noreply.github.com>
2022-06-07 02:53:11 -0600

    Patched a major bug in which the framerate would not reset after an action.

--- src/main/java/com/puradox/mouserateframerate/MouserateFramerateClient.java
+++ src/main/java/com/puradox/mouserateframerate/MouserateFramerateClient.java
@@ -20,12 +20,13 @@ public class MouserateFramerateClient implements ClientModInitializer {
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> { //For attaining the original FPS.
             AtomicReference<Double> defaultFPS = new AtomicReference<>(Option.FRAMERATE_LIMIT.get(client.options));
             ClientLifecycleEvents.CLIENT_STOPPING.register(client1 -> { //Reset framerate to original value on game close.
+                ticksSinceAction=0;
                 Option.FRAMERATE_LIMIT.set(client.options, defaultFPS.get());
             });
 
             ClientTickEvents.END_CLIENT_TICK.register(client1 -> { //Every tick
                 if (client.player!=null) {
-                    if(Option.FRAMERATE_LIMIT.get(client.options)!=defaultFPS.get() && Option.FRAMERATE_LIMIT.get(client.options)!=10) {
+                    if(Option.FRAMERATE_LIMIT.get(client.options)!=defaultFPS.get() && Option.FRAMERATE_LIMIT.get(client.options)!=Configuration.getInstance().frameDropMaximumFrames) {
                         defaultFPS.set(Option.FRAMERATE_LIMIT.get(client.options));
                     }
                     if (client.player.hurtTime > 0 || client.player.input.getMovementInput().length()>0 || client.mouse.getX()!=prevMouseX || client.mouse.getY()!=prevMouseY) {
